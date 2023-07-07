a4fc76e727e38e7c2f267f723c2a7ba30ef5a0cd
toftpokk <pakkapol.20100@gmail.com>
2023-07-07 23:37:34 +0700

    Fix: page starts at 1

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,7 +1,10 @@
 import { list_work } from "../lib/server/db";
 
 export function load({url}){
-    const page = Number(url.searchParams.get("page"))
+    let page = Number(url.searchParams.get("page"))
+    if(page == 0){
+        page = 1
+    }
     return {
         page: page,
         work: list_work(page)

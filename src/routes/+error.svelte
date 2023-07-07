b7aab9da42bd859165893de66052dc5984f41b66
toftpokk <pakkapol.20100@gmail.com>
2023-07-07 20:05:16 +0700

    Feature: Error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,10 @@
+<script>
+	import { page } from '$app/stores';
+</script>
+
+<main class="w-full my-6">
+	<div class="max-w-4xl block mx-auto my-8 text-xl">
+		<h1 class="text-2xl font-bold mb-4">{$page.status}</h1>
+		<p class="text-2xl mb-4">{$page.error?.message}</p>
+	</div>
+</main>

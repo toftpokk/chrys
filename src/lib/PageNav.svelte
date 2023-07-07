03ca259da2465b302ced5a695e9377cc8ad6d254
toftpokk <pakkapol.20100@gmail.com>
2023-07-07 22:01:04 +0700

    Feature: PageNav

--- src/lib/PageNav.svelte
+++ src/lib/PageNav.svelte
@@ -1,3 +1,20 @@
-<script>
+<script lang="ts">
+    export let page : number;
+    export let max : number;
+    export let url : string;
+    let leftHidden = true;
+    let rightHidden = true;
+
+    if(page > 1){
+        leftHidden = false
+    }
+    if(max < 0 || page < max){
+        rightHidden = false
+    }
 
 </script>
+<div class="max-w-7xl flex justify-center text-4xl mx-auto my-2">
+    <a class="w-8 {leftHidden? "hidden":""}" href={url+"?page="+(page-1)}>&larr;</a>
+    <span class="text-3xl w-10 mx-6 text-center">{page}</span>
+    <a class="w-8 {rightHidden? "hidden":""}" href={url+"?page="+(page+1)}>&rarr;</a>
+</div>

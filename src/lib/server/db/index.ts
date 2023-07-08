2136cd0e081ca2194ee59d3f042bd0e0a4b63323
toftpokk <pakkapol.20100@gmail.com>
2023-07-08 20:47:46 +0700

    Fix: installed better-sqlite and adapter-node

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -1,9 +1,7 @@
-import sqlite from 'sqlite3'
+import Database from 'better-sqlite3'
 import type { author, db_work, work } from '../../types'
 import { database_file, image_server, page_size } from '$lib/consts'
-
-sqlite.verbose()
-const db = new sqlite.Database(database_file)
+const db = new Database(database_file)
 
 function paginate(page: number){
     const start = (page-1)*page_size
@@ -11,57 +9,18 @@ function paginate(page: number){
     return {start,end}
 }
 
-function get(sql: string, params: any[]) {
-    return new Promise<any>((resolve, reject) => {
-        db.get(sql, params, (err, res) => {
-            if (err) {
-                reject(err)
-            }
-            else {
-                resolve(res)
-            }
-        })
-    })
-}
-
-function all(sql: string, params: any[]) {
-    return new Promise<any[]>((resolve, reject) => {
-        db.all(sql, params, (err, res) => {
-            if (err) {
-                reject(err)
-            }
-            else {
-                resolve(res)
-            }
-        })
-    })
-}
-
-function run(sql: string, params: any[]) {
-    return new Promise<{id:number}>((resolve, reject) => {
-        db.run(sql, params, function (err) { // use function to get lastID
-            if (err) {
-                reject(err)
-            }
-            else {
-                resolve({ id: this.lastID })
-            }
-        })
-    })
-}
-
 function create_author() {
-    const sql = `
+    const sql = db.prepare(`
     CREATE TABLE IF NOT EXISTS author (
         author_id INTEGER PRIMARY KEY AUTOINCREMENT,
         name TEXT VARCHAR(50),
         path VARCHAR(50)
-    )`
-    db.run(sql)
+    )`)
+    sql.run()
 }
 
 function create_work() {
-    const sql = `
+    const sql = db.prepare(`
     CREATE TABLE IF NOT EXISTS work (
         work_id INTEGER PRIMARY KEY AUTOINCREMENT,
         name VARCHAR(50),
@@ -71,8 +30,8 @@ function create_work() {
         favorite INTEGER(1),
         tags VARCHAR(100),
         FOREIGN KEY(author_id) REFERENCES author(author_id)
-    )`
-    db.run(sql)
+    )`)
+    sql.run()
 }
 
 export async function init_table() {
@@ -89,21 +48,21 @@ async function sync(work_list : Record<string,string[]>) {
     const author_names = Object.keys(work_list)
     author_names.map(async (author_name)=>{
         let author_id = -1
-        const author = await db_author_by_name(author_name)
+        const author = db_author_by_name(author_name)
         if(typeof author === "object"){
             author_id = author.author_id
         }
         else{
-            author_id = await db_insert_author(author_name,author_name)
+            author_id = db_insert_author(author_name,author_name)
         }
         work_list[author_name].map(async (work_name)=>{
             let work_id = -1
-            const work = await db_work_by_name_author(work_name,author_id)
+            const work = db_work_by_name_author(work_name,author_id)
             if(typeof work === "object"){
                 work_id = work.work_id
             }
             else{
-                work_id = await db_insert_work(work_name, `${author_name}/${work_name}`,author_id,[])
+                work_id = db_insert_work(work_name, `${author_name}/${work_name}`,author_id,[])
             }
         })
     })
@@ -136,57 +95,58 @@ async function scan(){
     return work_list
 }
 
-// DATABASE OPERATIONS
+// // DATABASE OPERATIONS
 
-export async function db_insert_author(name: string, path: string): Promise<number> {
-    const sql = 'INSERT INTO author (name,path) VALUES (?,?)'
-    const result = await run(sql, [name, path])
-    return result.id
+export function db_insert_author(name: string, path: string): number {
+    const info = db.prepare('INSERT INTO author (name,path) VALUES (?,?)')
+                   .run([name, path])
+    return Number(info.lastInsertRowid)
 }
 
-export async function db_insert_work(name: string, path: string, author_id: number,tags: string[]) {
-    const sql = 'INSERT INTO work (name,path,author_id,viewed,favorite,tags) VALUES (?,?,?,?,?,?)'
+export function db_insert_work(name: string, path: string, author_id: number,tags: string[]) : number {
     const t = tags.join(" ")
-    const result  = await run(sql, [name, path, author_id, false, false,t])
-    return result.id
+    const info = db.prepare('INSERT INTO work (name,path,author_id,viewed,favorite,tags) VALUES (?,?,?,?,?,?)')
+                   .run([name, path, author_id, 0, 0,t])
+    return Number(info.lastInsertRowid)
 }
 
-export function db_author_by_name(name: string): Promise<author | undefined> {
-    const sql = 'SELECT * FROM author WHERE name = ?'
-    return get(sql, [name])
+export function db_author_by_name(name: string): author | undefined {
+    const a : any = db.prepare('SELECT * FROM author WHERE name = ?')
+                           .get([name])
+    return a
 }
 
-export function db_work_by_name_author(name: string, author_id: number): Promise<db_work | undefined> {
-    const sql = 'SELECT * FROM work WHERE name = ? AND author_id = ?'
-    return get(sql, [name,author_id])
+export function db_work_by_name_author(name: string, author_id: number): db_work | undefined {
+    const w : any= db.prepare('SELECT * FROM work WHERE name = ? AND author_id = ?')
+                           .get([name,author_id])
+    return w
 }
 
-export function db_work_by_id(work_id: number): Promise<db_work | undefined>{
-    const sql = 'SELECT * FROM work WHERE work_id = ?'
-    return get(sql, [work_id])
+export function db_work_by_id(work_id: number): db_work | undefined{
+    const w : any = db.prepare('SELECT * FROM work WHERE work_id = ?')
+                      .get([work_id])
+    return w
 }
 
-export async function db_work_author_by_id(work_id:number) : Promise<any | undefined>{
-    const sql = `
+export function db_work_author_by_id(work_id:number) : any | undefined{
+    const w : any = db.prepare(`
     SELECT w.work_id,w.name,w.path,w.author_id,w.favorite,w.viewed,w.tags,a.name AS author_name
     FROM work w
     LEFT JOIN author a
     ON w.author_id = a.author_id
     WHERE w.work_id = ?
-    `
-    return await get(sql,[work_id])
-
+    `).get([work_id])
+    return w
 }
 
-export async function db_work_author() : Promise<work[]>{
-    const sql = `
+export function db_work_author() : work[]{
+    const ws : any[] = db.prepare(`
     SELECT w.work_id,w.name,w.path,w.author_id,w.favorite,w.viewed,w.tags,a.name AS author_name
     FROM work w
     LEFT JOIN author a
     ON w.author_id = a.author_id
-    `
-    return await all(sql,[])
-
+    `).all([])
+    return ws
 }
 
 async function get_images(author_name:string,work_name:string) : Promise<string[]>{
@@ -203,8 +163,9 @@ async function get_images(author_name:string,work_name:string) : Promise<string[
     }
     return images
 }
+
 export async function get_work(work_id: number) : Promise<work|null> {
-    const work = await db_work_author_by_id(work_id)
+    const work = db_work_author_by_id(work_id)
     if(typeof work === "object"){
         work.images = await get_images(work.author_name,work.name)
         if(work.tags === ''){
@@ -219,7 +180,7 @@ export async function get_work(work_id: number) : Promise<work|null> {
 }
 
 export async function list_work(page: number) : Promise<work[]>{
-    const partial_works = await db_work_author()
+    const partial_works = db_work_author()
     const {start,end} = paginate(page)
     const works = partial_works.slice(start,end)
     for(let w of works){

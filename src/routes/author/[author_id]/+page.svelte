a6da192b0f825fa3ef1607c5a6cbcd1f2bf9b35b
toftpokk <pakkapol.20100@gmail.com>
2023-07-15 13:23:03 +0700

    Refactor: remove unused imports

--- src/routes/author/[author_id]/+page.svelte
+++ src/routes/author/[author_id]/+page.svelte
@@ -1,12 +1,6 @@
 <script lang="ts">
-    import Card from '$lib/Card.svelte'
-    import PageNav from '$lib/PageNav.svelte';
     import Footer from '$lib/Footer.svelte'
     import type { work } from '$lib/types'
-	import { PUBLIC_IMAGE_REPO, PUBLIC_IMAGE_SERVER } from '$env/static/public';
-	import { page } from '$app/stores';
-	import { goto } from '$app/navigation';
-	import { browser } from '$app/environment';
 	import CardList from '$lib/CardList.svelte';
     
 	export let data : {work: work[], author_name: string};
--- src/routes/tag/[tag_name]/+page.svelte
+++ src/routes/tag/[tag_name]/+page.svelte
@@ -1,10 +1,7 @@
 <script lang="ts">
 	import { page } from '$app/stores';
-    import Card from '$lib/Card.svelte'
-    import PageNav from '$lib/PageNav.svelte';
     import Footer from '$lib/Footer.svelte'
     import type { work } from '$lib/types'
-	import { PUBLIC_IMAGE_REPO, PUBLIC_IMAGE_SERVER } from '$env/static/public';
 	import CardList from '$lib/CardList.svelte';
     const tag_name = $page.params.tag_name
     

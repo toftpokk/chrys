dcbe035e0cb46e1f246cefc1ac50672c3f9368ff
toftpokk <pakkapol.20100@gmail.com>
2023-07-15 09:20:43 +0700

    Fix: tag api submission

---
+++ src/routes/api/[work_id]/tag/+server.ts
@@ -0,0 +1,10 @@
+import { setTag } from '$lib/server/db/index.js'
+import { json } from '@sveltejs/kit'
+
+export const POST = async ({request,params})=>{
+    const work_id = Number(params.work_id)
+    const tags = await request.json()
+    await setTag(work_id,tags.join(" "))
+    console.log(tags)
+    return json({})
+}
--- src/routes/work/[work_id]/edit/+page.svelte
+++ src/routes/work/[work_id]/edit/+page.svelte
@@ -5,12 +5,25 @@
     const tag_string = work.tags.join(' ')
 
     import Footer from '$lib/Footer.svelte'
+
+    function handleSubmit(this: HTMLFormElement){
+        const data = new FormData(this);
+        const tag_string = String(data.get("tags"))
+        const tags = tag_string ? tag_string.split(' ').filter(t=>t!='') : [];
+        fetch(`/api/${work.work_id}/tag`,{
+            method: 'POST',
+            body: JSON.stringify(tags),
+            headers: {
+                'Content-Type' : 'application/json'
+            }
+        })
+    }
 </script>
 <main class="w-full my-6" >
     <div class="max-w-4xl block mx-auto my-8 text-xl">
         <h1 class="text-2xl font-bold mb-4">{work.name}</h1>
         <h2 class="mb-3 font-semibold">by {work.author_name}</h2>
-        <form method="POST" action="?/update_tag">
+        <form method="POST" on:submit|preventDefault={handleSubmit}>
             <textarea class="h-32 bg-light" name="tags" value={tag_string}/>
             <br/>
             <input class="bg-light font-bold my-3 px-2 py-2 rounded-sm cursor-pointer" type="submit" value="Submit"/>

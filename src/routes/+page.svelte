e67feef08818c3dac6b27e9915dbca6fa593a825
toftpokk <pakkapol.20100@gmail.com>
2023-07-31 11:18:14 +0700

    Fix: alphabet picker anchor tag

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,7 +9,7 @@
     <Header/>
     <ol class="flex justify-start flex-wrap max-w-7xl mx-auto">
         {#each data.alpha as char}
-            <li class="inline bg-mid px-[1ch] my-[0.5ch] mx-[0.5ch]"><a href={"/alpha/"+encodeURIComponent(char)}>{char}</a></li>
+        <li class="inline my-[0.5ch]"><a class="bg-mid px-[1ch] mx-[0.5ch] py-[0.3ch]" href={"/alpha/"+encodeURIComponent(char)}>{char}</a></li>
         {/each}
     </ol>
     <CardList data={data} frameLoad={true}/>

84f909f78dda0c3510f193937af69424b1793540
toftpokk <pakkapol.20100@gmail.com>
2023-08-07 11:30:51 +0700

    Fix: nav hover color

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -10,17 +10,18 @@
     ]
 </script>
 <style>
-    .icon { 
+    .link { 
+        color: black;
         filter: invert(100%);
     }
-    .icon:hover { 
+    .link:hover { 
         filter: invert(50%);
     }
 </style>
 <div class="bg-main mb-5 overflow-scroll">
     <ul class="block max-w-7xl w-max mx-auto">
         {#each links as link}
-            <li class="m-4 inline-block">
+            <li class="m-4 inline-block link">
                 <a class="text-xl inline" href={link.url}>
                     <img class="w-5 icon inline" src={link.icon} alt={link.name}>
                     <span class="text-md hidden sm:inline">{link.name}</span>

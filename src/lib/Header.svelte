a1230ae9e1ee8937ee11fc031abcc4cf05571fa8
toftpokk <pakkapol.20100@gmail.com>
2023-08-07 11:21:35 +0700

    Feat: header icons

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -1,11 +1,32 @@
+<script>
+    const links = [
+        {icon:"/icons/index.svg",url:"/",name:"Index"},
+        {icon:"/icons/authors.svg",url:"/author",name:"Authors"},
+        {icon:"/icons/tags.svg",url:"/tag",name:"Tags"},
+        {icon:"/icons/alphabet.svg",url:"/alphabet",name:"Alphabet"},
+        {icon:"/icons/history.svg",url:"/history",name:"History"},
+        {icon:"/icons/export.svg",url:"/export",name:"Export"},
+        {icon:"/icons/about.svg",url:"/about",name:"About"},
+    ]
+</script>
+<style>
+    .icon { 
+        filter: invert(100%);
+    }
+    .icon:hover { 
+        filter: invert(50%);
+    }
+</style>
 <div class="bg-main overflow-scroll">
     <ul class="block max-w-7xl w-max mx-auto">
-        <li class="m-4 inline-block"><a class="text-xl" href="/">Index</a></li>
-        <li class="m-4 inline-block"><a class="text-xl" href="/author">Authors</a></li>
-        <li class="m-4 inline-block"><a class="text-xl" href="/tag">Tags</a></li>
-        <li class="m-4 inline-block"><a class="text-xl" href="/alpha">Alphabet</a></li>
-        <li class="m-4 inline-block"><a class="text-xl" href="/history">History</a></li>
-        <li class="m-4 inline-block"><a class="text-xl" href="/export">Export</a></li>
-        <li class="m-4 inline-block"><a class="text-xl" href="/about">About</a></li>
+        {#each links as link}
+            <li class="m-4 inline-block">
+                <a class="text-xl inline" href={link.url}>
+                    <img class="w-5 icon inline" src={link.icon} alt={link.name}>
+                    <span class="text-md">{link.name}</span>
+                </a>
+            </li>
+        {/each}
+    
     </ul>
 </div>

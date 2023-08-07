41524a05f7d075a2dd2e0a1d92749befd21060ee
toftpokk <pakkapol.20100@gmail.com>
2023-08-07 10:50:52 +0700

    Fix: header overflow

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -1,11 +1,11 @@
-<div class="bg-main  flex justify-center mx-auto py-2 mb-10">
-    <ul class="w-full max-w-7xl block py-2">
-        <li class="m-4 inline"><a class="text-xl" href="/">Index</a></li>
-        <li class="m-4 inline"><a class="text-xl" href="/author">Authors</a></li>
-        <li class="m-4 inline"><a class="text-xl" href="/tag">Tags</a></li>
-        <li class="m-4 inline"><a class="text-xl" href="/alpha">Alphabet</a></li>
-        <li class="m-4 inline"><a class="text-xl" href="/history">History</a></li>
-        <li class="m-4 inline"><a class="text-xl" href="/export">Export</a></li>
-        <li class="m-4 inline"><a class="text-xl" href="/about">About</a></li>
+<div class="bg-main overflow-scroll">
+    <ul class="block max-w-7xl w-max mx-auto">
+        <li class="m-4 inline-block"><a class="text-xl" href="/">Index</a></li>
+        <li class="m-4 inline-block"><a class="text-xl" href="/author">Authors</a></li>
+        <li class="m-4 inline-block"><a class="text-xl" href="/tag">Tags</a></li>
+        <li class="m-4 inline-block"><a class="text-xl" href="/alpha">Alphabet</a></li>
+        <li class="m-4 inline-block"><a class="text-xl" href="/history">History</a></li>
+        <li class="m-4 inline-block"><a class="text-xl" href="/export">Export</a></li>
+        <li class="m-4 inline-block"><a class="text-xl" href="/about">About</a></li>
     </ul>
 </div>

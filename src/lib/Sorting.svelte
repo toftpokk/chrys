7d04ff10e661a75e55ac957162da0f155590e2b0
toftpokk <pakkapol.20100@gmail.com>
2023-08-07 09:25:17 +0700

    Fix: page sort history change

--- src/lib/Sorting.svelte
+++ src/lib/Sorting.svelte
@@ -2,23 +2,23 @@
 	import { goto } from '$app/navigation';
     import { page } from '$app/stores';
 	import { sort_type } from "./consts";
-	import { browser } from '$app/environment';
     const searchParams = $page.url.searchParams
     
-    let current_sort = "default"
-    $: if (browser){
-    let searchParamSort = searchParams.get("sort")
-        if (current_sort !== searchParamSort){
-            if (!(current_sort === "default" && searchParamSort === null)){
-                searchParams.set("sort",current_sort)
-                goto("?"+searchParams.toString())
-            }
-        }
+    let current_sort : string;
+
+    $:{
+        current_sort  = searchParams.get("sort") || "default";
+    }
+    function change_sort(){
+        const u = new URLSearchParams($page.url.searchParams)
+        u.set("page","1") // reset page
+        u.set("sort",current_sort)
+        goto("?"+u.toString())
     }
 </script>
 <div class="max-w-7xl mx-auto flex justify-start py-3">
     <label class="text-xl mx-2" for="sort">Sort:</label>
-    <select name="sort" id="sort" class="bg-main px-3 py-2" bind:value={current_sort}>
+    <select name="sort" id="sort" class="bg-main px-3 py-2" bind:value={current_sort} on:change={change_sort}>
         {#each sort_type as t }
             <option value={t}>{t}</option>
         {/each}

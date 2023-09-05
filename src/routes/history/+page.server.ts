43b476b7de4dd34ddee0e4e6e5456eac92c0707d
toftpokk <pakkapol.20100@gmail.com>
2023-09-05 12:21:35 +0700

    Feat: history sorting

--- src/routes/history/+page.server.ts
+++ src/routes/history/+page.server.ts
@@ -2,6 +2,8 @@ import { select_histories } from '$lib/server/db/database.js';
 
 
 export function load({url}){
-    const history = select_histories()
+    const history = select_histories().sort((a,b)=>{
+        return a.datetime < b.datetime ? 1 : -1
+    })
     return {history}
 }

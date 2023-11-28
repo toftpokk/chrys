3f0152b5f32cc1e1d9652e4f73f8ed051480da2f
toftpokk <pakkapol.20100@gmail.com>
2023-11-28 19:57:47 +0700

    Feat: update about page

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -9,16 +9,14 @@
         <h1 class="text-2xl font-bold mb-4">Inspired by </h1>
         <ul class="mb-8">
             <li>Cubari.moe <a class="underline" href="https://cubari.moe">https://cubari.moe</a></li>
-            <li>Stash (18+) <a class="underline" href="https://stashapp.cc">https://stashapp.cc</a></li>
-            <li>Nhentai (18+) <a class="underline" href="https://nhentai.net">https://nhentai.net</a></li>
         </ul>
         <h1 class="text-2xl font-bold mb-4">Developer</h1>
         <h2 class="mb-3 ml-3 font-semibold">toftpokk</h2>
         <ul class="mb-4 ml-6">
             <li class="h-12 block align-middle">
-                <a class="inline pl-3" href="https://github.com/kong-wep">
+                <a class="inline pl-3" href="https://github.com/toftpokk">
                     <img class="h-10 inline" src="/github-logo-white.svg"/>
-                    <span class="inline pl-2">https://github.com/kong-wep</span>
+                    <span class="inline pl-2">https://github.com/tofptokk</span>
                 </a>
             </li>
             <li class="h-14 block align-middle">

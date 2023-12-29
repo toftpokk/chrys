bbb50e791147e03e22d152651dafc608a4540277
toftpokk <pakkapol.20100@gmail.com>
2023-12-30 00:51:34 +0700

    Feat: series grouping

--- src/lib/CardList.svelte
+++ src/lib/CardList.svelte
@@ -3,17 +3,34 @@
 	import Card from "./Card.svelte";
     import PageNav from "./PageNav.svelte";
 	import Sorting from "./Sorting.svelte";
+	import { IsEmptySeries } from "./helper";
 	import type { work } from "./types";
 
     export let data: {work: work[], num_pages?: number}
     export let hasNav = true;
     export let filterView = false;
+    export let groupSeries = false; // set to default false
 
     let page_max = -1;
-    let work : work[]= data.work
+    let series : string[] = []
+    let work= data.work
+    if (groupSeries){
+        work = work.filter(filterDuplicateSeries)
+    }
     if(data.num_pages) page_max = data.num_pages
+
+    function filterDuplicateSeries(w : work){
+        if(IsEmptySeries(w.series)){
+            return true
+        }
+        if(series.includes(w.series)){
+            return false
+        }
+        series.push(w.series)
+        return true
+    }
 </script>
-<div class="max-w-7xl mx-auto">
+<div class="max-w-7xl mx-auto" data-sveltekit-reload> <!-- Or else page wont load-->
     {#if hasNav}
         <PageNav max={page_max}/>
         <Sorting filterView={filterView}/>
@@ -33,6 +50,8 @@
                 viewed={w.viewed}
                 favorite={w.favorite}
                 author_id={w.author_id}
+                isSeries={groupSeries? !IsEmptySeries(w.series): false}
+                series={w.series}
             />
         {/each}
     </ul>

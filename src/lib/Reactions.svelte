9855bd3179b5c5c21bcbbc20075ef58fd527f93b
toftpokk <pakkapol.20100@gmail.com>
2023-12-29 15:51:45 +0700

    Fix: remove bookmark feature

--- src/lib/Reactions.svelte
+++ src/lib/Reactions.svelte
@@ -3,19 +3,19 @@
     const dispatch = createEventDispatcher()
     export let favorite = false
     export let viewed = false
-    export let bookmark = false
+    // export let bookmark = false
     const toggleFav = ()=>{
         dispatch('toggleFav')
     }
     const toggleView = ()=>{
         dispatch('toggleView')
     }
-    const toggleBookmark = ()=>{
-        dispatch('toggleBookmark')
-    }
+    // const toggleBookmark = ()=>{
+    //     dispatch('toggleBookmark')
+    // }
 </script>
 <div class="flex my-1">
     <input on:click={toggleFav} type="image" alt="favorite" class="cursor-pointer h-7 my-3 mx-2" src={favorite ? "/icon-heart-on.svg": "/icon-heart.svg"}/>
-    <input on:click={toggleBookmark} type="image" alt="favorite" class="cursor-pointer h-7 my-3 mx-2" src={bookmark ? "/icon-bookmark-on.svg": "/icon-bookmark.svg"}/>
+    <!-- <input on:click={toggleBookmark} type="image" alt="favorite" class="cursor-pointer h-7 my-3 mx-2" src={bookmark ? "/icon-bookmark-on.svg": "/icon-bookmark.svg"}/> -->
     <input on:click={toggleView} type="image" alt="viewed" class="cursor-pointer h-7 my-3 mx-2" src={viewed ? "/icon-eye-on.svg": "/icon-eye.svg"}/>
 </div>

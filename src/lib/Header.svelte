c3e86ffc3517b9ce9ab3eb20d28ba99144964308
toftpokk <pakkapol.20100@gmail.com>
2024-02-19 12:06:58 +0700

    Fix header path color

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -9,17 +9,18 @@
     import iconSettings from "$lib/icon/settings.svg?raw"
     import iconAbout from "$lib/icon/about.svg?raw"
 	import { page } from "$app/stores";
-    const current_page = $page.url.pathname;
+    let current_page = ""
+    $: $page.url && (current_page = $page.url.pathname.split('/')[1]);
 </script>
 <div class="bg-main mb-5 overflow-hidden">
     <ul class="block max-w-7xl w-max mx-auto">
-        <HeaderItem name="Index" url="/" icon={iconIndex} currentPage={current_page}/>
-        <HeaderItem name="Authors" url="/author" icon={iconAuthors} currentPage={current_page}/>
-        <HeaderItem name="Tags" url="/tag" icon={iconTags} currentPage={current_page}/>
-        <HeaderItem name="Search" url="/alpha" icon={iconSearch} currentPage={current_page}/>
-        <HeaderItem name="History" url="/history" icon={iconHistory} currentPage={current_page}/>
-        <HeaderItem name="Series" url="/series" icon={iconFolder} currentPage={current_page}/>
-        <HeaderItem name="Settings" url="/settings" icon={iconSettings} currentPage={current_page}/>
-        <HeaderItem name="About" url="/about" icon={iconAbout} currentPage={current_page}/>
+        <HeaderItem name="Index" url="/" icon={iconIndex} active={current_page == ""}/>
+        <HeaderItem name="Authors" url="/author" icon={iconAuthors} active={current_page == "author"}/>
+        <HeaderItem name="Tags" url="/tag" icon={iconTags} active={current_page == "tag"}/>
+        <HeaderItem name="Search" url="/alpha" icon={iconSearch} active={current_page == "alpha"}/>
+        <HeaderItem name="History" url="/history" icon={iconHistory} active={current_page == "history"}/>
+        <HeaderItem name="Series" url="/series" icon={iconFolder} active={current_page == "series"}/>
+        <HeaderItem name="Settings" url="/settings" icon={iconSettings} active={current_page == "settings"}/>
+        <HeaderItem name="About" url="/about" icon={iconAbout} active={current_page == "about"}/>
     </ul>
 </div>
--- src/lib/HeaderItem.svelte
+++ src/lib/HeaderItem.svelte
@@ -1,15 +1,13 @@
 <script lang="ts">
     import Icon from '$lib/Icon.svelte'
     export let icon : string
-    export let currentPage : string
+    export let active : boolean = false
     export let name : string
     export let url : string
-
-    const isCurrentUrl = currentPage == url
 </script>
-<li class="m-3 inline-block link text-white" class:text-gray-20={isCurrentUrl}>
+<li class="m-3 inline-block link text-white" class:text-gray-20={active}>
     <a class="text-lg" href={url}>
-        <Icon src={icon} color={isCurrentUrl ? "#949daa" : "#FFF"} size={16}/>
+        <Icon src={icon} color={active ? "#949daa" : "#FFF"} size={16}/>
         <span class="align-middle hidden md:inline">{name}</span>
     </a>
 </li>

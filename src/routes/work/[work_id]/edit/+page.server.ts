bda70210e4389087d9838cea9457120c77be2de2
toftpokk <pakkapol.20100@gmail.com>
2024-04-19 11:28:02 +0700

    Fix list_series promise

--- src/routes/work/[work_id]/edit/+page.server.ts
+++ src/routes/work/[work_id]/edit/+page.server.ts
@@ -4,7 +4,7 @@ export const load = async ({params})=>{
     const work_id = Number(params.work_id)
     const work = await get_work(work_id)
     const all_work_tags = list_tags()
-    const series = list_series()
+    const series = await list_series()
     return {
         all_work_tags,
         work,
